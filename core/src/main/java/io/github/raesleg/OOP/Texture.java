6683388c350c6f5871c696804cf7dbad51b41eae
mockkk <mockyj03@gmail.com>
2026-02-05 01:19:27 +0800

    this code quite messy, have another code called "TextureEntity"

--- core/src/main/java/io/github/raesleg/OOP/Texture.java
+++ core/src/main/java/io/github/raesleg/OOP/Texture.java
@@ -5,18 +5,35 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public abstract class Texture extends Entity{
 
     /* Private Variables */
-    private Texture texture;
+    private com.badlogic.gdx.graphics.Texture texture;
 
     /* Public Functions */
-    public void Tex(){};
+    public void Tex() {
+        texture = null;
+    };
 
-    public void Tex(String filename, float x, float y, float speed) {};
+    public void Tex(String filename, float x, float y, float speed) {
+        setX(x);
+        setY(y);
+        setSpeed(speed);
+        texture = new com.badlogic.gdx.graphics.Texture(filename);
+    };
 
-    public Texture getTexture() {return texture;};
+    public com.badlogic.gdx.graphics.Texture getTexture() {
+        return texture;
+    };
 
-    public void movement(){};
+    public void movement() { };
 
-    public void draw(SpriteBatch batch){};
+    public void draw(SpriteBatch batch){
+        if (texture != null) {
+            batch.draw(texture, getX(), getY());
+        }
+    };
 
-    public void dispose() {};
+    public void dispose() {
+        if (texture != null) {
+            texture.dispose();
+        }
+    };
 }
