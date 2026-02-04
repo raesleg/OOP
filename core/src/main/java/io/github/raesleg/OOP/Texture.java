606ddb0f932b1b9447994e30d4d30a525d620eec
Jes000 <93638361+Jes000@users.noreply.github.com>
2026-02-05 03:31:38 +0800

    Refactor Texture class to TextureObject
    Renamed cause name conflict, added the super from TextureEntity

--- core/src/main/java/io/github/raesleg/OOP/Texture.java
+++ core/src/main/java/io/github/raesleg/OOP/Texture.java
@@ -1,39 +1,42 @@
 package io.github.raesleg.OOP;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public abstract class Texture extends Entity{
+public abstract class TextureObject extends Entity{
 
     /* Private Variables */
-    private com.badlogic.gdx.graphics.Texture texture;
+    private Texture texture;
 
     /* Public Functions */
-    public void Tex() {
-        texture = null;
-    };
+    public TextureObject() {
+        super();
+        this.texture = null;
+    }
 
-    public void Tex(String filename, float x, float y, float speed) {
-        setX(x);
-        setY(y);
-        setSpeed(speed);
-        texture = new com.badlogic.gdx.graphics.Texture(filename);
-    };
+    public TextureObject(String filename, float x, float y, float speed) {
+        super(x, y, speed);
+        texture = new Texture(Gdx.files.internal(filename));
+    }
 
-    public com.badlogic.gdx.graphics.Texture getTexture() {
+    public Texture getTexture() {
         return texture;
-    };
+}
 
-    public void movement() { };
+    public void movement() { }
 
-    public void draw(SpriteBatch batch){
+    @Override
+    public void draw(SpriteBatch batch) {
         if (texture != null) {
             batch.draw(texture, getX(), getY());
         }
-    };
+    }
 
+    @Override
     public void dispose() {
         if (texture != null) {
             texture.dispose();
         }
-    };
+    }
 }
