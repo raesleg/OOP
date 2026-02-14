4171393ad7f5f6286e37d7f56694a848471ac72e
mockkk <mockyj03@gmail.com>
2026-02-14 15:17:14 +0800

    Updated SoundEffect.java

--- core/src/main/java/io/github/raesleg/engine/sound/SoundEffect.java
+++ core/src/main/java/io/github/raesleg/engine/sound/SoundEffect.java
@@ -8,13 +8,31 @@ public class SoundEffect {
   // Declare sound variable to hold the sound effect
   private Sound sound;
 
+  // Declare loopId to manage looping state (default - not looping)
+  private long loopId = -1;
+
   public SoundEffect(String filePath) {
     this.sound = Gdx.audio.newSound(Gdx.files.internal(filePath));
   }
 
-  // Method to play the sound effect
+  // Play the sound effect
   public void play() {
     sound.play();
+  }
+
+  // Loop the sound effect
+  public void loop() {
+    if (loopId == -1) {
+        loopId = sound.loop();
+    }
+  }
+
+  // Stop the sound effect
+  public void stop() {
+    if (loopId != -1) {
+        sound.stop(loopId);
+        loopId = -1;
+    }
   }
 
   public void dispose() {
